12fabeee37cbec8a1759a599455856abdedb975e
Mappleaf <pxpxpx2015@gmail.com>
2025-07-13 03:19:47 +0300

    Clown (me) isn't testing his releases
    ... and pushing it to the stable branch 🤡🤡🤡🤡🤡

--- compat/fixes/fixes_updates.lua
+++ compat/fixes/fixes_updates.lua
@@ -1,1 +1,3 @@
 require "more_ore.more_ore_updates"
+require "more_alloy.more_alloy_updates"
+require "more_galore.more_galore_updates"
--- compat/fixes/more_galore/more_galore_updates.lua
+++ compat/fixes/more_galore/more_galore_updates.lua
@@ -1,7 +1,16 @@
 if mods["more-ore"] then
     -- zinc
-    local bronze_proc_tech_preqs = data.raw["technology"]["bronze-processing"]["prerequisites"]
+    local bronze_proc_tech = data.raw["technology"]["bronze-processing"]
+    local bronze_proc_tech_preqs = bronze_proc_tech["prerequisites"]
     local idx = 1
+
+    bronze_proc_tech["unit"] = nil
+    bronze_proc_tech["research_trigger"] = {
+        type = "craft-item",
+        item = "tin-plate",
+        count = 1
+    }
+
 
     for _, preq in pairs(bronze_proc_tech_preqs) do
         if preq == "automation-science-pack" then
